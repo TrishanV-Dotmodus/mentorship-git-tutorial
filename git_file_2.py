eb1c87535e27138ce0a548ab171afb768d38e424
TrishanConnector97 <trishan.vassan@dentsuaegis.com>
2021-07-26 15:19:39 +0200

    Modified new files

--- git_file_2.py
+++ git_file_2.py
@@ -1,6 +1,6 @@
 print('file 2')
 
 # - Make the change (add/deleting/updating files, code etc)
-# - Commiting the changes ( git add . (. means all changes) , single add example (git add git_file_1.py)  ,
-# git commit -m "message")
+# - Add the changes ( git add . (. means all changes) , single add example (git add git_file_1.py)
+# - Commit changes : ( git commit -m "message")
 # - pushing
