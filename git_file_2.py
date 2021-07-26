f4e1e10fcc7633073f4d23edabc79e28e8c6fa86
TrishanConnector97 <trishan.vassan@dentsuaegis.com>
2021-07-26 15:18:12 +0200

    Adding additional files to repo

---
+++ git_file_2.py
@@ -0,0 +1,6 @@
+print('file 2')
+
+# - Make the change (add/deleting/updating files, code etc)
+# - Commiting the changes ( git add . (. means all changes) , single add example (git add git_file_1.py)  ,
+# git commit -m "message")
+# - pushing
