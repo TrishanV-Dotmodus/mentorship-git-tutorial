c77f74ba29c2fd147d0d525311b19ae2d547ae69
Phokwane_Malebye <phokwanemalebye@gmail.com>
2021-07-27 13:20:49 +0200

    Adding, deleting, modifying files for my first PR

--- git_file_2.py
+++ git_file_2.py
@@ -3,4 +3,5 @@ print('file 2')
 # - Make the change (add/deleting/updating files, code etc)
 # - Add the changes ( git add . (. means all changes) , single add example (git add git_file_1.py)
 # - Commit changes : ( git commit -m "message")
-# - pushing
+# - pushing(git push ,copy provided link to push upstream -only when a new branch is created locall)
+
