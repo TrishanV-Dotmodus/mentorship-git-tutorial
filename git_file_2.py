7a3549f2a9cf729c064dd8dabddef7939b812bf7
TrishanConnector97 <trishan.vassan@dentsuaegis.com>
2021-08-04 14:27:28 +0200

    Second branch to cause merge conflict

--- git_file_2.py
+++ git_file_2.py
@@ -3,5 +3,5 @@ print('file 2')
 # - Make the change (add/deleting/updating files, code etc)
 # - Add the changes ( git add . (. means all changes) , single add example (git add git_file_1.py)
 # - Commit changes : ( git commit -m "message")
-# - pushing(git push ,copy provided link to push upstream -only when a new branch is created locall)
+# - pushing(git push ,copy provided link to push upstream -only when a new branch is created local)
 
