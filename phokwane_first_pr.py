2baeeaef7665f4b906460c1646d4564b595d6c18
Phokwane_Malebye <phokwanemalebye@gmail.com>
2021-08-09 22:03:58 +0200

    Adding a new file, deleting and modifying an existing file

--- phokwane_first_pr.py
+++ phokwane_first_pr.py
@@ -1,1 +1,3 @@
 #The first I created
+#updating this file with a new comment as per the assessment requirements
+#the comment seems a bit long
---
+++ question_2_add_file.py
@@ -0,0 +1,9 @@
+#file added in the repo
+
+print("Mentorship assessment based on Git")
+#We have gone through git basics extensively
+#Some Git commands include
+#git clone
+#git add .
+#git commit -m " comments"
+#git push
